44d7a29ff3d8adeb264760e6eaa63b4fa2cad51c
ayoub.labite@gmail.com <ayoub.labite@gmail.com>
2025-12-09 14:44:02 +0100

    TG-73: Modèle Utilisateur avec rôles (MongoDB/Mongoose)

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,3 +1,15 @@
 import Express  from "express";
 import cors from cors;
 import dotenv from "dotenv";
+import { connectDB } from "./config/database.js";
+
+dotenv.config();
+const app = Express();
+app.use(cors());
+app.use(Express.json());
+connectDB();
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Serveur en cours d'exécution sur le port ${PORT}`);
+});
